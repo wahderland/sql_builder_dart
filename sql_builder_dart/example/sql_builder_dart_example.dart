94383414a23aa95e67472c163cd9844b76d2136e
wahderland <service@wahderland.com>
2022-05-24 23:46:41 +0800

    init project

---
+++ sql_builder_dart/example/sql_builder_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:sql_builder_dart/sql_builder_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ sql_builder_dart/test/sql_builder_dart_test.dart
@@ -0,0 +1,16 @@
+import 'package:sql_builder_dart/sql_builder_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
